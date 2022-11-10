85f90872c46f03ac4420db5035b605c6ae1b1fcf
Matuto Delivery <112352298+AppMatutoDelivery@users.noreply.github.com>
2022-11-10 18:06:01 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,25 @@
-self.addEventListener('fetch', function(event) {
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
 
+// Caching the listed asset below
+const assetsToCache = [
+  './',
+  './OneSignalSDKWorker.js',
+  './index.html',
+  './styles.css',
+  './icon.png',
+  './manifest.json',
+  './service-worker.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(CacheHelper.deleteOldCache());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
